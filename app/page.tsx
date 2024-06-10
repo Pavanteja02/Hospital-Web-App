235b6d3f3ace5f9de2d642106f5f903b8513c48c
Pavanteja02 <npt12@iitbbs.ac.in>
2024-06-10 17:56:10 +0530

    added search in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CategorySearch from "./_components/CategorySearch";
 import Hero from "./_components/Hero";
 
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <Hero/>
+      <CategorySearch/>
     </div>
   );
 }
