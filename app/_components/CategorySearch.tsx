e2e03bacd2d25ba3157736bfc2c26127586b984d
Pavanteja02 <npt12@iitbbs.ac.in>
2024-06-10 19:27:50 +0530

    added different categories on homepage from strapi api

--- app/_components/CategorySearch.tsx
+++ app/_components/CategorySearch.tsx
@@ -1,11 +1,28 @@
+"use client"
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Search } from 'lucide-react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import GlobalApi from '../_utils/GlobalApi'
+import Image from 'next/image'
 
 function CategorySearch() {
+
+    const [categoryList,setCategoryList]=useState([]);
+    useEffect(()=>{
+        getCategoryList()
+    },[])
+
+
+    const getCategoryList=()=>{
+        GlobalApi.getCategory().then((resp: any)=>{
+            console.log(resp.data.data);
+            setCategoryList(resp.data.data);
+        })
+    }
+
   return (
-    <div className='mb-10 items-center flex flex-col gap-2'>
+    <div className='mb-10 items-center px-5 flex flex-col gap-2'>
         <h2 className='font-bold
         text-4xl tracking-wide'>
             Search <span className='text-primary'> Doctors</span></h2>
@@ -18,6 +35,20 @@ function CategorySearch() {
         Search</Button>
     </div>
 
+    <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
+    {categoryList.map((item,index)=>index<6 && (
+        <div key={index} className='flex flex-col text-center
+        items-center p-5 bg-blue-50 m-2 rounded-lg cursor-pointer
+        gap-2 hover:scale-110 transition-all erase-in-out'>
+            <Image src={item.attributes?.Icon?.data?.attributes?.url}
+            alt='icon'
+            width={40}
+            height={40}/>
+            <label className='text-blue-600 text-sm'>{item.attributes?.Name}</label>
+        </div>
+
+    ))}
+    </div>
     </div>
   )
 }
